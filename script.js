3448d94e253f0bba93d5c2b0403d4e98d7676891
alexhardo <alexhardo@msn.com>
2018-04-23 11:38:46 +0200

    second exercise

--- script.js
+++ script.js
@@ -10,10 +10,20 @@ function crystalGazer(ar1, ar2, ar3, ar4) {
     } else if (ar1 < 0) {
         alert("nobody has a minus kid");
     }
-
-    var msg = "You will be a  " + ar4 + " in " + ar3 + " and married to" + ar2 + " with " + ar1 + " child " + "<br>";
-
-
+    var msg = "You will be a  " + ar4 + " in " + ar3 + " and married to " + ar2 + " with " + ar1 + " child " + "<br>";
     document.getElementById("outputtxt").innerHTML = msg;
+}
+
+
+function AgeCalculator(by1) {
+
+    var d = new Date();
+    var current = d.getFullYear();
+    var by = document.getElementById("by1").value;
+    var birthyear = Number(by);
+    var age = current - birthyear;
+    var age1 = age -1;
+    var msg1 = "You are either  " + age + "  years old or "+age1+"";
+    document.getElementById("outputtxt1").innerHTML = msg1;
 
 }
